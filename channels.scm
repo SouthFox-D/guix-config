d2302af01d27d7eaf9f93af590b73f3d96581ba7
SouthFox <master@southfox.me>
2024-11-26 15:16:47 +0800

    [feat] use mirror to avoid DDOS...
    Signed-off-by: SouthFox <master@southfox.me>

--- channels.scm
+++ channels.scm
@@ -1,4 +1,7 @@
-(append (list (channel
-                (name 'fox-channel)
-                (url "https://git.southfox.me/southfox/guix-config")))
-        %default-channels)
+(list (channel
+       (inherit (car %default-channels))
+       (url "https://git.southfox.me/mirror/guix"))
+      (channel
+       (name 'fox-channel)
+       (url "https://git.southfox.me/southfox/guix-config"))
+      )
