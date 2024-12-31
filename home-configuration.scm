92ec98b9e2123b7859fc78571879e43e22425533
SouthFox <master@southfox.me>
2025-01-01 01:23:09 +0800

    [fix] use absolute path for bin

--- home-configuration.scm
+++ home-configuration.scm
@@ -56,7 +56,7 @@
                   (".config/hypr/hyprlock.conf" ,(local-file "files/hyprlock.conf"))
                   (".config/hypr/hyprpaper.conf" ,(local-file "files/hyprpaper.conf"))
                   ;; bin
-                  (".local/bin" ,(local-file "files/bin" #:recursive? #t))
+                  (".local/bin/chill-player" ,(local-file "files/bin/chill-player" #:recursive? #t))
                   ))
        (service home-zsh-service-type
                 (home-zsh-configuration
