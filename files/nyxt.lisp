5c45b08209c874ff850f85c722853ff6f55794df
SouthFox <master@southfox.me>
2025-03-06 01:25:35 +0800

    [feat/nyxt] update default-mode and key-map config

--- files/nyxt.lisp
+++ files/nyxt.lisp
@@ -1,8 +1,11 @@
-(define-configuration buffer
-  ((default-modes
-    (pushnew 'nyxt/mode/vi:vi-normal-mode %slot-value%))))
+(define-configuration web-buffer
+    ((default-modes
+      (pushnew 'nyxt/mode/vi:vi-normal-mode %slot-value%))))
 
 (define-configuration buffer
     ((override-map
       (let ((map (make-keymap "override-map")))
-        (define-key map "M-x" 'execute-command)))))
+        (define-key map "M-x" 'execute-command)
+        (define-key map "C-d" 'scroll-page-down)
+        (define-key map "C-u" 'scroll-page-up)
+        ))))
