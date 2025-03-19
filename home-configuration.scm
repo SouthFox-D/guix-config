5ef7c5878b8f8d5250572f9b83c99fe81a051707
SouthFox <master@southfox.me>
2025-03-19 14:01:25 +0800

    [feat/home] add condition for lighthouse host

--- home-configuration.scm
+++ home-configuration.scm
@@ -32,7 +32,13 @@
        (service oh-my-zsh-service-type)
        (service home-zsh-service-type
                 (home-zsh-configuration
-                 (zshrc (list (eval-file "files/zshrc"))))))))
+                 (zshrc (list (eval-file "files/zshrc")))))))
+     (service home-files-service-type
+              `((".config/zellij/config.kdl" ,(eval-file "files/zellij.kdl"))
+
+                ;; bin
+                (".local/bin/flk" ,(local-file "files/bin/flk.sh" #:recursive? #t))
+                )))
 
     (home-environment
      (packages (append (specifications->packages
