bb3765760cc232267bfa56e4430a0df2464d903a
SouthFox <master@southfox.me>
2025-03-25 17:10:20 +0800

    [feat] add work-machine condition

--- common.scm
+++ common.scm
@@ -1,3 +1,6 @@
 (define touchable-machine?
   (not (or (member (gethostname) (list "mastfox" "basefox" "alifox" "txfox"))
            (equal? "lighthouse" (getlogin)))))
+
+(define work-machine?
+  (equal? "n100" (gethostname)))
