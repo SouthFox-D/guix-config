f7dbdb310bf655fcc0bb6efa92e04f6951004ed5
SouthFox <master@southfox.me>
2025-05-25 00:51:38 +0800

    [package] add zerotier
    Adopted from nonguix

--- modules/fox/packages.scm
+++ modules/fox/packages.scm
@@ -16,6 +16,7 @@
   #:use-module (gnu packages python-xyz)
   #:use-module (gnu home services)
   #:use-module (guix build-system copy)
+  #:use-module (guix build-system gnu)
   #:use-module (guix build-system pyproject)
   #:use-module (gnu services))
 
@@ -189,3 +190,79 @@ and others.")
     (description
      "Anki's shared backend and web components, and the Qt frontend.")
     (license agpl3+)))
+
+(define license (@@ (guix licenses) license))
+
+(define* (nonfree uri #:optional (comment ""))
+  "Return a nonfree license, whose full text can be found
+at URI, which may be a file:// URI pointing the package's tree."
+  (license "Nonfree"
+           uri
+           (string-append
+            "This a nonfree license.  Check the URI for details.  "
+            comment)))
+
+(define-public zerotier
+  (package
+    (name "zerotier")
+    (version "1.14.2")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://github.com/zerotier/ZeroTierOne")
+                    (commit version)))
+              (file-name (git-file-name name version))
+              (sha256
+               (base32
+                "0dxm67c06hg538z8adxgj5zqxmgrwhnlwznpi3sk2qvfms6zzvhg"))))
+    (build-system gnu-build-system)
+    (arguments
+     `(#:make-flags (list "ZT_SSO_SUPPORTED=0") ; We don't need SSO/OIDC
+       #:phases
+       (modify-phases %standard-phases
+         ;; There is no ./configure
+         (delete 'configure)
+         (replace 'check
+           (lambda* (#:key make-flags #:allow-other-keys)
+             (apply invoke "make" "selftest" make-flags)
+             (invoke "./zerotier-selftest")))
+         (replace 'install
+           (lambda* (#:key outputs #:allow-other-keys)
+             (let* ((out (assoc-ref outputs "out"))
+                    (sbin (string-append out "/sbin"))
+                    (lib (string-append out "/lib"))
+                    (man (string-append out "/share/man"))
+                    (zerotier-one-lib (string-append lib "/zerotier-one")))
+               (mkdir-p sbin)
+               (install-file "zerotier-one" sbin)
+               (with-directory-excursion sbin
+                 (symlink (string-append sbin "/zerotier-one") "zerotier-cli")
+                 (symlink (string-append sbin "/zerotier-one") "zerotier-idtool"))
+
+               (mkdir-p zerotier-one-lib)
+               (with-directory-excursion zerotier-one-lib
+                 (symlink (string-append sbin "/zerotier-one") "zerotier-one")
+                 (symlink (string-append sbin "/zerotier-one") "zerotier-cli")
+                 (symlink (string-append sbin "/zerotier-one") "zerotier-idtool"))
+
+               (mkdir-p (string-append man "/man8"))
+               (install-file "doc/zerotier-one.8" (string-append man "/man8"))
+
+               (mkdir-p (string-append man "/man1"))
+               (for-each (lambda (man-page)
+                           (install-file man-page (string-append man "/man1")))
+                         (list "doc/zerotier-cli.1"
+                               "doc/zerotier-idtool.1"))
+               #t))))))
+    (home-page "https://github.com/zerotier/ZeroTierOne")
+    (synopsis "Smart programmable Ethernet switch for planet Earth")
+    (description "It allows all networked devices, virtual machines,
+containers, and applications to communicate as if they all reside in the same
+physical data center or cloud region.
+
+This is accomplished by combining a cryptographically addressed and secure
+peer to peer network (termed VL1) with an Ethernet emulation layer somewhat
+similar to VXLAN (termed VL2).  Our VL2 Ethernet virtualization layer includes
+advanced enterprise SDN features like fine grained access control rules for
+network micro-segmentation and security monitoring.")
+    (license (nonfree "https://mariadb.com/bsl11/"))))
