50b26ab654bb6eb7a096170289748c309c8d8ffe
SouthFox <master@southfox.me>
2025-11-26 11:53:04 +0800

    [refactor/package] split to binary module

---
+++ modules/fox/packages/licenses.scm
@@ -0,0 +1,14 @@
+(define-module (fox packages licenses)
+  #:use-module (guix licenses)
+  #:export (nonfree))
+
+(define license (@@ (guix licenses) license))
+
+(define* (nonfree uri #:optional (comment ""))
+  "Return a nonfree license, whose full text can be found
+at URI, which may be a file:// URI pointing the package's tree."
+  (license "Nonfree"
+           uri
+           (string-append
+            "This a nonfree license.  Check the URI for details.  "
+            comment)))
