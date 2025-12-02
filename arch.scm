e35df45602961f7a53f5b139b59abeab0ce1a88a
SouthFox <master@southfox.me>
2025-12-02 20:34:38 +0800

    [fix] add missing semicolon

--- arch.scm
+++ arch.scm
@@ -271,7 +271,7 @@
                                 (nginx-location-configuration
                                  (uri (string-append "~ ^/" pi-webhook-path "/(.*)$"))
                                  (body `(,(string-append "rewrite ^/" pi-webhook-path "/(.*)$ /hooks/$1 break;")
-                                          "proxy_pass http://127.0.0.1:8082"
+                                          "proxy_pass http://127.0.0.1:8082;"
                                           "proxy_set_header Host $host;"
                                           "proxy_set_header X-Real-IP $remote_addr;"
                                           "proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;"
